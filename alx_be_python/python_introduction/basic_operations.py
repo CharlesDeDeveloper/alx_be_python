7eef401e01c5d751fe2130eceda846336b924711
Charles Okoli <charlesosita318@gmail.com>
2025-09-25 01:22:32 +0100

    Add basic_operations script

---
+++ alx_be_python/python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+nunber1 = 10
+number2 = 5
+sum_result = nunber1 + number2
+difference_result = nunber1 - number2
+product_result = nunber1 * number2
+print("Addition of number1 and number2 is", sum_result)
+print("subtraction of number1 and number2 is", difference_result)
+print("multiplication of number1 and number2 is", product_result)
